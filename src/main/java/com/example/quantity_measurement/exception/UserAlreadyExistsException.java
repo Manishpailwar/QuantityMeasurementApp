4be2754a85a90f0d9c5985aed0a91fd9406e5818
Manish Pailwar <ram823890@gmail.com>
2026-04-15 14:40:12 +0530

    intial commit

--- src/main/java/com/example/quantity_measurement/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.quantity_measurement.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/quantity_measurement/repository/UserManagementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.quantity_measurement.repository;
-
-import com.example.quantity_measurement.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserManagementRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
